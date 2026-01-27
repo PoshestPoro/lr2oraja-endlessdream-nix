1a75b0b1c652fd4b85c458717f90dac6ec010825
PoshPoro <poshestporo1@proton.me>
2026-01-27 23:30:23 +0000

    reformat

--- flake.nix
+++ flake.nix
@@ -1,27 +1,26 @@
 {
+  description = "lr2oraja-endlessdream flake!";
+
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs =
-    { self
-    , nixpkgs
-    , flake-utils
-    ,
-    }:
-    flake-utils.lib.eachDefaultSystem (
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs {
+        pkgs = import inputs.nixpkgs {
           inherit system;
         };
-        callPackage = pkgs.lib.callPackageWith (pkgs // packages);
-        packages = {
-          libjportaudio = callPackage ./libjportaudio/default.nix { };
-          lr2oraja-endlessdream = callPackage ./lr2oraja-endlessdream/default.nix { };
-        };
       in
-      packages
+      {
+        packages = {
+          lr2oraja-endlessdream = pkgs.callPackage ./pkgs/lr2oraja-endlessdream/default.nix
+            {
+              libjportaudio = (pkgs.callPackage ./pkgs/libjportaudio/default.nix { });
+            };
+        };
+      }
     );
 }
