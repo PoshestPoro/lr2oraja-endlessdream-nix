03d0cb7a97806ddefdef4b2e1cee1988b88e3dbb
PoshPoro <poshestporo1@proton.me>
2026-01-29 01:52:42 +0000

    remove beatoraja dependency

--- pkgs/lr2oraja-endlessdream/default.nix
+++ pkgs/lr2oraja-endlessdream/default.nix
@@ -1,6 +1,6 @@
 { lib
 , stdenv
-, fetchzip
+, fetchFromGitHub
 , fetchurl
 , gtk3
 , jdk
@@ -69,65 +69,67 @@ stdenv.mkDerivation
         	    done < <(echo "''${JDK_JAVA_OPTIONS}" | tr ' ' '\n')
         	  fi
         	}
-        	dataDir="''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"
+        	dataDir="''${XDG_DATA_HOME:-$HOME/.local/share}/lr2oraja-endlessdream"
         	if [ ! -d "$dataDir" ]; then
         	  mkdir -p "$dataDir"
-        	  cp -r $out/opt/beatoraja/* "$dataDir"
+        	  cp -r $out/opt/lr2oraja-endlessdream/* "$dataDir"
         	  find "$dataDir" -type f -exec chmod 644 {} \;
         	  find "$dataDir" -type d -exec chmod 755 {} \;
         	fi
-        	cd "''${XDG_DATA_HOME:-$HOME/.local/share}/beatoraja"
+        	cd "''${XDG_DATA_HOME:-$HOME/.local/share}/lr2oraja-endlessdream"
 
         	JDK_JAVA_OPTIONS=$(build_java_options) \
         	SHUT_UP_TACHI=yes \
         	  ${finalAttrs.jdkFx}/bin/java \
         	    -Xms1g -Xmx4g \
-        	    -cp $out/opt/lr2oraja-endlessdream/beatoraja.jar:ir/* bms.player.beatoraja.MainLoader \
+        	    -cp lr2oraja-endlessdream.jar:ir/* bms.player.beatoraja.MainLoader \
         	    "$@"
 
       '';
 
-    lr2oraja-jar = fetchurl
+    src = fetchurl
       {
-        url = "https://github.com/seraxis/lr2oraja-endlessdream/releases/download/v${finalAttrs.lr2oraja-endlessdream-version}/lr2oraja-${finalAttrs.beatoraja-version}-endlessdream-linux-${finalAttrs.lr2oraja-endlessdream-version}.jar";
-
-        hash = "sha256:5623cca355e1c350c5e7bef4d512b875d3fb700c73e60e2289c9a0ad5ef7d28e";
+        url = "https://github.com/seraxis/lr2oraja-endlessdream/releases/download/v0.4.0/lr2oraja-0.8.8-endlessdream-linux-0.4.0.jar";
+        hash = "sha256-ViPMo1Xhw1DF57701RK4ddP7cAxz5g4iicmgrV730o4=";
       };
 
-    src = fetchzip
-      {
-        url = "https://mocha-repository.info/download/beatoraja${finalAttrs.beatoraja-version}.zip";
-        hash = "sha256-xR7tlGr0t82CfGgegp3l+euFf7CvpPzU5x6lDkd99XA=";
-      };
     icon = fetchurl
       {
         url = "https://aur.archlinux.org/cgit/aur.git/plain/lr2oraja-endlessdream-icon.png?h=lr2oraja-endlessdream";
         hash = "sha256-/b03/0OqavIPnrZDvycad+9XkBSXCno9zs945lEj2D0=";
       };
+    gitsrc = fetchFromGitHub {
+      owner = "seraxis";
+      repo = "lr2oraja-endlessdream";
+      rev = "${finalAttrs.lr2oraja-endlessdream-version}";
+      hash = "sha256-e9yjO1jbfPKPgozlURP7Sv3ci14T/1sj+1Cl1IYQgsY=";
+    };
 
     nativeBuildInputs = [ makeWrapper copyDesktopItems ];
-
+    unpackPhase = ":";
 
     installPhase = ''
-                  mkdir -p $out/opt/lr2oraja-endlessdream
-                  mkdir -p $out/bin
-                  ln -s ${finalAttrs.startupScript} $out/bin/lr2oraja-endlessdream
-                  mv * $out/opt/lr2oraja-endlessdream/
+      		runHook preInstall
+                              mkdir -p $out/opt/lr2oraja-endlessdream
+                              mkdir -p $out/bin
+                              ln -s ${finalAttrs.startupScript} $out/bin/lr2oraja-endlessdream
+                              mv * $out/opt/lr2oraja-endlessdream/
 
-                  cp ${finalAttrs.lr2oraja-jar} $out/opt/lr2oraja-endlessdream/beatoraja.jar
-                  install -Dm644 ${finalAttrs.icon} $out/share/icons/lr2oraja-endlessdream-icon.png
+                              cp ${finalAttrs.src} $out/opt/lr2oraja-endlessdream/lr2oraja-endlessdream.jar
+            		cp -r ${finalAttrs.gitsrc}/assets/* "$out/opt/lr2oraja-endlessdream/"
+                              install -Dm644 ${finalAttrs.icon} $out/share/icons/lr2oraja-endlessdream-icon.png
 
-                  wrapProgram $out/bin/lr2oraja-endlessdream \
-                    --set out $out \
-                    --suffix PATH : "${finalAttrs.binPath}" \
-                    --prefix LD_LIBRARY_PATH : "${addDriverRunpath.driverLink}/lib:${finalAttrs.libPath}" \
-      	      --prefix XDG_DATA_DIRS : ${gtk3}/share/gsettings-schemas/* \
+                              wrapProgram $out/bin/lr2oraja-endlessdream \
+                                --set out $out \
+                                --suffix PATH : "${finalAttrs.binPath}" \
+                                --prefix LD_LIBRARY_PATH : "${addDriverRunpath.driverLink}/lib:${finalAttrs.libPath}" \
+                  	      --prefix XDG_DATA_DIRS : ${gtk3}/share/gsettings-schemas/* \
 
-                  runHook postInstall
+                              runHook postInstall
     '';
     meta = with lib; {
       description = "Cross-platform rhythm game based on Java and libGDX.";
-      homepage = "https://github.com/exch-bms2/beatoraja";
+      homepage = "https://github.com/seraxis/lr2oraja-endlessdream";
       license = [ licenses.gpl3Only "unknown" ];
       platforms = [ "x86_64-linux" ];
       mainProgram = "lr2oraja-endlessdream";
